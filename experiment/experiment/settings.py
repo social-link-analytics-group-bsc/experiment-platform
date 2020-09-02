cfa7a7ca3d7c45439f0d2d166173a4475ef70f8b
orpuig <orpuig@gmail.com>
2020-09-02 10:03:31 +0200

    Add app to the project and prepare to use session cache

--- experiment/experiment/settings.py
+++ experiment/experiment/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'expplat.apps.ExpplatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
