3b7d3dec5db470f9c5d11198a6ba639763b7e47f
orpuig <orpuig@gmail.com>
2020-09-02 11:39:44 +0200

    Add models to admin site

--- experiment/expplat/admin.py
+++ experiment/expplat/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Experiment, News, User, QuestionType, Question, Choice, QuestionExperiment, Answer
+
+admin.site.register(Experiment)
+admin.site.register(News)
+admin.site.register(User)
+admin.site.register(QuestionType)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(QuestionExperiment)
+admin.site.register(Answer)
