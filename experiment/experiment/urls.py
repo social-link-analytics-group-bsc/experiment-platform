1cf4e36f683de1ecaa6d686f6a995abfde597dd2
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-10-27 15:15:07 +0100

    Add url paths of expplat for root

--- experiment/experiment/urls.py
+++ experiment/experiment/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('expplat.urls'))
     path('admin/', admin.site.urls),
     path('expplat/', include('expplat.urls'))
 ]
