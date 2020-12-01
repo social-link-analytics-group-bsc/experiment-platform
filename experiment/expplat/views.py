d93209e5494d2dfb1f4cdb4a880e1407bc3d49de
orpuig <orpuig@gmail.com>
2020-12-01 16:19:09 +0100

    Saving IP address anonymously. Related to #79

--- experiment/expplat/views.py
+++ experiment/expplat/views.py
@@ -47,7 +47,17 @@ def saveTimes(request, current):
     usr.save()
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def index(request):
+
+    # get_client_ip(request) TODO save it somewhere anonymously
 
     if 'state' in request.session.keys():
         request.session.flush()
